748273e75ffc6289c57670d9103ebffc6093b71e
almendra flores <almendra flores>
2017-05-15 15:52:53 -0500

    CommitFinal

---
+++ 2010204834/EmpresaTransporte.Entities/Entity/TipoPago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaTransporte.Entities
+{
+    public enum TipoPago : byte
+    {
+        Contado=1,
+        Credito=2
+    }
+}
